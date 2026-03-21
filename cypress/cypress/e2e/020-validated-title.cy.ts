95791a271448fa429a2911c739bfc1f70226c8ca
Marty Trujillo <marty@ilikeemail.com>
2026-03-21 12:27:18 -0400

    Issue 15 - Makes the e2e base URL configurable.

--- cypress/cypress/e2e/020-validated-title.cy.ts
+++ cypress/cypress/e2e/020-validated-title.cy.ts
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost:4200/');
+    cy.visit('/');
     cy.contains('Say Hello Coming Soon...')
   .should('be.visible')
   })
